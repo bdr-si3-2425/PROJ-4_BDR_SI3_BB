761a367649092ba413a422c7edb313bcb2f1be6e
Enzo Miech <enzomiech@gmail.com>
2025-02-05 09:39:20 +0100

    changement de creation_des_tables

--- requetes/creation_des_tables.sql
+++ requetes/creation_des_tables.sql
@@ -1,13 +1,13 @@
 -- Table des bibliothèques
 CREATE TABLE Bibliotheque (
-    id_bibliotheque SERIAL PRIMARY KEY,
+    id_bibliotheque INT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
     region VARCHAR(255) NOT NULL
 );
 
 -- Table des ouvrages
 CREATE TABLE Ouvrage (
-    id_ouvrage SERIAL PRIMARY KEY,
+    id_ouvrage INT PRIMARY KEY,
     titre VARCHAR(255) NOT NULL,
     auteur VARCHAR(255) NOT NULL,
     genre VARCHAR(255) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE Ouvrage (
 
 -- Table des exemplaires
 CREATE TABLE Exemplaire (
-    id_exemplaire SERIAL PRIMARY KEY,
+    id_exemplaire INT PRIMARY KEY,
     id_ouvrage INT REFERENCES Ouvrage(id_ouvrage) ON DELETE CASCADE,
     id_bibliotheque INT REFERENCES Bibliotheque(id_bibliotheque) ON DELETE CASCADE,
 	emplacement TEXT NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE Abonnement (
 
 -- Table des abonnés
 CREATE TABLE Abonne (
-    id_abonne SERIAL PRIMARY KEY,
+    id_abonne INT PRIMARY KEY,
 	id_abonnement INT REFERENCES Abonnement(id_abonnement),
 	nom VARCHAR(255) NOT NULL,
 	prenom VARCHAR(255),
@@ -43,7 +43,7 @@ CREATE TABLE Abonne (
 
 -- Table des prêts
 CREATE TABLE Pret (
-    id_pret SERIAL PRIMARY KEY,
+    id_pret INT PRIMARY KEY,
     id_abonne INT REFERENCES Abonne(id_abonne) ON DELETE CASCADE,
     id_exemplaire INT REFERENCES Exemplaire(id_exemplaire) ON DELETE CASCADE,
     date_emprunt DATE NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE Pret (
 
 -- Tables des reservations
 CREATE TABLE Reservation (
-    id_reservation SERIAL PRIMARY KEY,
+    id_reservation INT PRIMARY KEY,
     id_abonne INT REFERENCES Abonne(id_abonne) ON DELETE CASCADE,  
     id_exemplaire INT REFERENCES Exemplaire(id_exemplaire) ON DELETE CASCADE, 
 	id_bibliotheque_destination INT REFERENCES Bibliotheque(id_bibliotheque) ON DELETE CASCADE,
@@ -63,7 +63,7 @@ CREATE TABLE Reservation (
 
 -- Table des distances 
 CREATE TABLE Distance (
-	id_distance SERIAL PRIMARY KEY,
+	id_distance INT PRIMARY KEY,
 	id_bibliotheque_source INT REFERENCES Bibliotheque(id_bibliotheque) ON DELETE CASCADE,
 	id_bibliotheque_destination INT REFERENCES Bibliotheque(id_bibliotheque) ON DELETE CASCADE,
 	distance INT NOT NULL
@@ -71,7 +71,7 @@ CREATE TABLE Distance (
 
 -- Table des transferts
 CREATE TABLE Transfert (
-    id_transfert SERIAL PRIMARY KEY,
+    id_transfert INT PRIMARY KEY,
 	id_reservation INT REFERENCES Reservation(id_reservation) ON DELETE CASCADE,
     id_distance INT REFERENCES Distance(id_distance) ON DELETE CASCADE,
 	date_demande DATE NOT NULL,
@@ -81,7 +81,7 @@ CREATE TABLE Transfert (
 
 -- Table des événements
 CREATE TABLE Evenement (
-    id_evenement SERIAL PRIMARY KEY,
+    id_evenement INT PRIMARY KEY,
     id_bibliotheque INT REFERENCES Bibliotheque(id_bibliotheque) ON DELETE CASCADE,
     nom_evenement VARCHAR(255) NOT NULL,
     date_evenement DATE NOT NULL,
