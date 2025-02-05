6e3fb54887faeab6eabb80af5c5371b9eaf77aea
adrien verstraelen <adrien.verstraelen@gmail.com>
2025-02-05 10:55:50 +0100

    Modification requete 16

--- requetes/requete16.sql
+++ requetes/requete16.sql
@@ -1,6 +1,9 @@
-Select o.titre,region,count(*) AS "nb_of_livres" from abonne AS a
-JOIN pret as p ON  p.id_abonne=a.id_abonne
-JOIN exemplaire AS e on e.id_exemplaire=p.id_exemplaire
-JOIN ouvrage as o ON o.id_ouvrage=e.id_ouvrage
-GROUP BY (region,o.titre)
-ORDER BY "nb_of_livres" DESC
+
+-- Affiche les livres les plus empruntés par régions
+
+SELECT region, o.titre, count(*) AS "nb_of_livres" FROM abonne AS a
+JOIN pret AS p ON  p.id_abonne=a.id_abonne
+JOIN exemplaire AS e ON e.id_exemplaire=p.id_exemplaire
+JOIN ouvrage AS o ON o.id_ouvrage=e.id_ouvrage
+GROUP BY region, o.titre
+ORDER BY region, "nb_of_livres" DESC
